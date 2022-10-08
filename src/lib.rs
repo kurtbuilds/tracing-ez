429e52015676ead50421751f9710afae0abaa5c7
Kurt Wolf <kurtwolfbuilds@gmail.com>
2022-10-08 10:40:19 -0400

    Docs

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,7 @@
 pub use tracing::span as tracing_span;
 pub use tracing::{info, warn, error, debug, Level};
-
+use tracing_subscriber::fmt;
+use std::io;
 
 #[macro_export]
 macro_rules! span {
@@ -14,7 +15,20 @@ macro_rules! span {
     }
 }
 
-pub fn set_global_default_stdout() -> Result<(), tracing::subscriber::SetGlobalDefaultError> {
-    let subscriber = ::tracing_subscriber::FmtSubscriber::new();
-    ::tracing::subscriber::set_global_default(subscriber)
+/// According to tracing-subscriber docs, init() is supposed to panic if
+/// another subscriber is already set. However, it does not. Instead, right
+/// now, the *first* subscriber is used (*not* the last).
+pub fn set_global_default_stdout() {
+    fmt()
+        .with_writer(io::stdout)
+        .init();
+}
+
+/// According to tracing-subscriber docs, init() is supposed to panic if
+/// another subscriber is already set. However, it does not. Instead, right
+/// now, the *first* subscriber is used (*not* the last).
+pub fn set_global_default_stderr() {
+    fmt()
+        .with_writer(io::stderr)
+        .init();
 }
