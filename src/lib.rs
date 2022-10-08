754c3a2c6b3cdb9653f9c05520648134d3887444
Kurt Wolf <kurtwolfbuilds@gmail.com>
2022-10-08 10:30:16 -0400

    Fix

--- src/lib.rs
+++ src/lib.rs
@@ -1,19 +1,21 @@
-pub use tracing::{info, warn, error, debug};
+pub mod export;
+pub use tracing::span as tracing_span;
+pub use tracing::{info, warn, error, debug, Level};
 
 
 #[macro_export]
 macro_rules! span {
     ($name:expr) => {
-        let span = tracing::span!(Level::INFO, $name);
+        let span = $crate::tracing_span!($crate::Level::INFO, $name);
         let _enter = span.enter();
     };
     ($name:expr, $($fields:tt)*) => {
-        tracing::span!(Level::INFO, $name, $($fields)*);
+        let span = $crate::tracing_span!($crate::Level::INFO, $name, $($fields)*);
         let _enter = span.enter();
     }
 }
 
 pub fn set_global_default_stdout() -> Result<(), tracing::subscriber::SetGlobalDefaultError> {
-    let subscriber = tracing_subscriber::FmtSubscriber::new();
-    tracing::subscriber::set_global_default(subscriber)
+    let subscriber = ::tracing_subscriber::FmtSubscriber::new();
+    ::tracing::subscriber::set_global_default(subscriber)
 }
